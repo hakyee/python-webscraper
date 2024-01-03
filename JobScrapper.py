0c1d05e2f45da6c1cbd2f6b6135bd34760cc9329
silizard <wkbkmj1@naver.com>
2024-01-03 17:33:00 +0900

    #7.3

--- JobScrapper.py
+++ JobScrapper.py
@@ -5,7 +5,15 @@ url ="https://weworkremotely.com/categories/remote-full-stack-programming-jobs"
 
 response = requests.get(url)
 
-soup = BeautifulSoup(response.content, "html.parser")
+soup = BeautifulSoup(response.content, "html.parser",)
 
-jobs = soup.find("section", class_="jobs").find_all("li")
-print(jobs)
+jobs = soup.find("section", class_="jobs").find_all("li")[1:-1]
+
+# job도 beatifulsoup의 object이므로 soup의 모든 기능을 사용할 수 있다.
+for job in jobs:
+    title = job.find("span", class_="title").text
+    company, position, region = job.find_all("span", class_="company")
+    company = company.text
+    position = position.text
+    region = region.text
+    print(title, company, position, region, "---------\n")
